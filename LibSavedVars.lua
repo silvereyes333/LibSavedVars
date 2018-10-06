1a9ca3a636631a8ce6720798b33ee6d9618f8537
silvereyes <silvereyes333@gmail.com>
2018-10-05 19:55:39 -0500

    Version 3.0
    - New methods New(), NewAccountWide() and NewCharacterIdSettings() return a data object that can be used as a 1:1 replacement for ZO_SavedVars instances
    - New helper method GetRawDataTable() returns the underlying data table for a ZO_SavedVars instance, since ZO_SavedVars:New() just returns an interface, not the data itself.
    - Added usage documentation / comments to all public methods in code.
    
    - Data class features:
      * data["key"] and data.key operator support
      * data["key"] = value and data.key = value operator support
      * next(), pairs() and ipairs() support for iterating saved vars
      * GetAccountSavedVarsActive() and SetAccountSavedVarsActive(value) methods for toggling the active scope between account and character-specific settings
      * Migrate(legacySavedVars, beforeCallback, addon, ...) method to call directly and control when migration is executed.
      * GetActiveSavedVars() method to return a reference to the active ZO_SavedVars instance for the currently logged-in character
      * GetLength() returns the # length of the data for the active ZO_SavedVars instance for the currently logged-in character
      * GetLibAddonMenuSetting() replaces the old library-wide method that took an "addon" parameter.
    
    - DEPRECATED METHODS:
      * Get() Use data["key"] or data.key of the new data class instead.
      * Set(value) Use data["key"] = value or data.key = value of the new data class instead.
      * Init() Legacy var migration moved to data class.  Use data:Migrate() of the new data class and New(), NewAccountWide() and NewCharacterIdSettings() instead.
      * GetLibAddonMenuSetting(addon) Use data:GetLibAddonMenuSetting() of the new data class instead.

--- LibSavedVars.lua
+++ LibSavedVars.lua
@@ -1,42 +1,239 @@
 --[[ LibSavedVars and its files © silvereyes
-     Distributed under MIT license (see LICENSE.txt) ]]
+     Distributed under MIT license (see LICENSE) ]]
 
 -- copy language strings locally, then destroy
 local libSavedVarsStrings = LIBSAVEDVARS_STRINGS
 LIBSAVEDVARS_STRINGS = nil
 
 --Register LibSavedVars with LibStub
-local MAJOR, MINOR = "LibSavedVars", 2
-local libSavedVars, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
+local LIBNAME, LIBVERSION = "LibSavedVars", 3.0
+local libSavedVars, oldminor = LibStub:NewLibrary(LIBNAME, LIBVERSION)
 if not libSavedVars then return end --the same or newer version of this lib is already loaded into memory
 
--- Server/world name registry
-local WORLDS = {
-    ["live"] = {
-        ["EU Megaserver"] = true,
-        ["NA Megaserver"] = true,
-    },
-    ["pts"] = {
-        ["PTS"] = true
-    },
-}
-local DO_NOT_OVERWRITE = true
+local deepSavedVarsCopy
 
 -- Create localized strings
 for stringId, value in pairs(libSavedVarsStrings) do
     ZO_CreateStringId(stringId, value)
 end
 
--- Clears all settings from the given 
+---------------------------------------
+--
+--       Public methods
+-- 
+---------------------------------------
+
+--[[
+     Clears all saved vars values from the given ZO_SavedVars instance. Leaves the built-in "version" var, and all
+     function definitions.
+     
+     savedVars: the ZO_SavedVars table to clear
+  ]]--
 function libSavedVars:ClearSavedVars(savedVars)
-    for key, value in pairs(getmetatable(savedVars).__index) do
+    local dataTable = self:GetRawDataTable(savedVars)
+    for key, value in pairs(dataTable) do
         if key ~= "version" and type(value) ~= "function" then
             savedVars[key] = nil
         end
     end
 end
 
-local function deepSavedVarsCopy(source, dest, doNotOverwrite)
+--[[
+     Copies saved var values from one ZO_SavedVars instance to another, optionally ignoring vars that already exist in
+     the destination.
+     
+     source:         The ZO_SavedVars instance to copy values from
+     destination:    The ZO_SavedVars instance to copy values to
+     doNotOverwrite: (optional) If true, only vars that equal nil in the destination will be copied. default: false.
+  ]]--
+function libSavedVars:DeepSavedVarsCopy(source, destination, doNotOverwrite)
+    
+    -- Get rid of the annoying ZO_SavedVars interface crap to deal with the data directly
+    source      = self:GetRawDataTable(source)
+    destination = self:GetRawDataTable(destination)
+    
+    -- Copy keys from source to destination
+    for key, value in pairs(source) do
+        
+        -- Copy nested tables
+        if type(value) == "table" then
+            if type(destination[key]) ~= "table" then
+                destination[key] = {}
+            end
+            self:DeepSavedVarsCopy(value, destination[key], doNotOverwrite)
+            
+        -- Copy scalar values
+        elseif key ~= "version" and type(value) ~= "function" then
+        
+            -- Make sure we don't overwrite destination values, if requested
+            if not doNotOverwrite or destination[key] == nil then
+                destination[key] = value
+            end
+        end
+    end
+end
+
+--[[
+     Gets the underlying data table for a ZO_SavedVars instance, since ZO_SavedVars don't support many common table
+     operations directly (e.g. pairs/ipairs/next/#).
+  ]]--
+function libSavedVars:GetRawDataTable(savedVars)
+    local meta = getmetatable(savedVars)
+    return meta and meta.__index or savedVars
+end
+
+--[[
+     Creates ZO_SavedVar instances for account-wide and/or character-specific saved vars, per-server, and returns a 
+     new data object (See Data.lua) that can be used just like a normal ZO_SavedVars object, but can toggle between
+     account-wide and character-specific settings on-the-fly.  
+     
+     The data object can also be used for migrating old ZO_SavedVar instances to the server-specific structure using the
+     data:Migrate() method.  See the Data.lua file for full documentation.
+     
+     accountSavedVarsName:          Name of the account-wide saved var to create, or nil.
+                                    If nil, then characterSavedVarsName should not be nil.
+                                    
+                                    Should match a name specified with ## SavedVariables in your manifest.
+     characterSavedVarsName:        Name of the character-specific saved var to create, or nil.
+                                    If nil, then accountSavedVarsName should not be nil.
+                                    Should match a name specified with ## SavedVariables in your manifest.
+                                    
+     defaults:                      (optional) Table containing default values for your saved vars.
+     
+     defaultAccountSavedVarsActive: True if account-wide saved vars should be enabled by default the first time settings
+                                    are loaded for a character; false if character-specific settings should be enabled
+                                    by default.
+                                    If you plan to use the libSavedVars_Data:Migrate() feature, you should pass in true
+                                    only if your legacy saved vars were created with ZO_SavedVars:NewAccountWide().
+                                    If your legacy saved vars were created with ZO_SavedVars:New() or 
+                                    ZO_SavedVars:NewCharacterIdSettings(), then you should pass in false.
+  ]]--
+function libSavedVars:New(accountSavedVarsName, characterSavedVarsName, defaults, useAccountSettingsDefault)
+    return self:GetClass("Data"):New(accountSavedVarsName, characterSavedVarsName, defaults, useAccountSettingsDefault)
+end
+
+--[[
+     Stand-in replacement for ZO_SavedVars:NewAccountWide()
+     Returns a LibSavedVars data object just like libSavedVars:New(), but with no character-specific settings.  
+     Mainly used to get a quick data file instance for legacy saved vars migration.
+  ]]--
+function libSavedVars:NewAccountWide(accountSavedVarsName, defaults)    
+    return self:GetClass("Data"):New(accountSavedVarsName, nil, defaults, true)
+end
+
+--[[
+     Stand-in replacement for ZO_SavedVars:NewCharacterIdSettings()
+     Returns a LibSavedVars data object just like libSavedVars:New(), but with no account-wide settings.  
+     Mainly used to get a quick data file instance for legacy saved vars migration.
+  ]]--
+function libSavedVars:NewCharacterIdSettings(characterSavedVarsName, defaults)
+    return self:GetClass("Data"):New(nil, characterSavedVarsName, defaults, false)
+end
+
+
+
+-------------------------------------------------------
+--
+--       Stubbed library class helper functions
+-- 
+-------------------------------------------------------
+
+-- Library class registry tables
+local classes       = { }
+local classVersions = { }
+
+--[[
+     Gets the active version of the given LibSavedVars class by name. 
+     Returns the class definition table and version number
+  ]]--
+function libSavedVars:GetClass(name)
+    return classes[name], classVersions[name]
+end
+
+--[[
+     Similar to LibStub:NewLibrary(), but used to break out LibSavedVars classes into separate files without version 
+     conflicts.
+  ]]--
+function libSavedVars:NewClass(name, version)
+    if not classVersions[name] or classVersions[name] < version then
+        classVersions[name] = version
+        classes[name] = ZO_Object:Subclass()
+        return classes[name]
+    end
+end
+
+
+
+----------------------------------------------------------------------------
+--
+--       Deprecated methods
+--
+--       The following methods will be removed in a future version.
+--       Please migrate to the new methods above and those in Data.lua
+-- 
+----------------------------------------------------------------------------
+
+-- Registry of data instances by addon for v2 backwards compatibility
+local addonInstances = { }
+
+--[[
+     DEPRECATED
+     See Data.lua => libSavedVars_Data:__index(key)
+  ]]--
+function libSavedVars:Get(addon, key)
+    if not addonInstances[addon] then return end
+    return addonInstances[addon][key]
+end
+
+--[[ 
+     DEPRECATED
+     See Data.lua => libSavedVars_Data:GetLibAddonMenuSetting(default)
+  ]]--
+function libSavedVars:GetLibAddonMenuSetting(addon, default)
+    if not addonInstances[addon] then return end
+    return addonInstances[addon]:GetLibAddonMenuAccountCheckbox(default)
+end
+
+--[[ 
+     DEPRECATED
+     See libSavedVars:New()
+     See Data.lua => libSavedVars_Data:Migrate(legacySavedVars, beforeCallback, addon, ...)
+  ]]--
+function libSavedVars:Init(addon, accountSavedVarsName, characterSavedVarsName, defaults, useAccountSettingsDefault,
+        legacySavedVars, legacyIsAccountWide, legacyMigrationCallback, ...)
+    
+    local data = self:New(accountSavedVarsName, characterSavedVarsName, defaults, useAccountSettingsDefault)
+    
+    addon.accountSettings   = data.dataSource.accountSavedVars
+    addon.characterSettings = data.dataSource.characterSavedVars
+    addonInstances[addon]   = data
+    
+    if legacySavedVars then
+        data:Migrate(legacySavedVars, legacyMigrationCallback, addon, ...)
+    end
+end
+
+--[[ 
+     DEPRECATED
+     See Data.lua => libSavedVars_Data:__newindex(key, value)
+  ]]--
+function libSavedVars:Set(addon, key, value)
+    if not addonInstances[addon] then return end
+    addonInstances[addon][key] = value
+end
+
+
+
+---------------------------------------
+--
+--       Private functions
+-- 
+---------------------------------------
+
+--[[
+     Helper method for libSavedVars:DeepSavedVarsCopy()
+  ]]--
+deepSavedVarsCopy = function(source, dest, doNotOverwrite)
     for key, value in pairs(source) do
         if type(value) == "table" then
             if type(dest[key]) ~= "table" then
@@ -49,87 +246,4 @@ local function deepSavedVarsCopy(source, dest, doNotOverwrite)
             end
         end
     end
-end
-
-function libSavedVars:DeepSavedVarsCopy(source, dest, doNotOverwrite)
-    deepSavedVarsCopy(getmetatable(source).__index, getmetatable(dest).__index, doNotOverwrite)
-end
-
-function libSavedVars:Get(addon, key)
-    if addon.characterSettings.useAccountSettings then
-        return addon.accountSettings[key]
-    else
-        return addon.characterSettings[key]
-    end 
-end
-
-function libSavedVars:GetLibAddonMenuSetting(addon, default)
-    -- Account-wide settings
-    return {
-        type    = "checkbox",
-        name    = GetString(SI_LSV_ACCOUNT_WIDE),
-        tooltip = GetString(SI_LSV_ACCOUNT_WIDE_TT),
-        getFunc = function() return addon.characterSettings.useAccountSettings end,
-        setFunc = function(value)
-            addon.characterSettings.useAccountSettings = value
-            -- When account-wide settings are the default and character-specific settings are chosen, 
-            -- copy any account-wide settings that are not defined on the character to the character-specific settings.
-            if default and not value then
-                self:DeepSavedVarsCopy(addon.accountSettings, addon.characterSettings, DO_NOT_OVERWRITE)
-            end
-        end,
-        default = default,
-    }
-end
-
-function libSavedVars:Init(addon, accountWideSavedVarsName, characterSavedVarsName, 
-                           defaults, useAccountSettingsDefault, 
-                           legacySavedVars, legacyIsAccountWide, legacyMigrationCallback)
-    
-    local accountWideDefaults = ZO_ShallowTableCopy(defaults)
-    accountWideDefaults["useAccountSettings"] = nil
-    useAccountSettingsDefault = useAccountSettingsDefault or defaults["useAccountSettings"]
-    
-    if useAccountSettingsDefault then
-        defaults = { ["useAccountSettings"] = true }
-    else
-        defaults["useAccountSettings"] = false
-    end
-    
-    local worldName = GetWorldName()
-    addon.accountSettings = ZO_SavedVars:NewAccountWide(accountWideSavedVarsName, 1, nil, accountWideDefaults, worldName)
-    addon.characterSettings = ZO_SavedVars:NewCharacterIdSettings(characterSavedVarsName, 1, nil, defaults, worldName)
-    
-    if not legacySavedVars or legacySavedVars.libSavedVarsMigrated then
-        return
-    end
-    
-    -- Migrate old settings to new world-specific settings
-    
-    if legacyMigrationCallback and type(legacyMigrationCallback) == "function" then
-        legacyMigrationCallback(addon, legacySavedVars)
-    end
-    local worlds = WORLDS["live"][worldName] and WORLDS["live"] or WORLDS["pts"]
-    
-    for copyToWorldName, _ in pairs(worlds) do
-        local settings
-        if copyToWorldName == worldName then
-            settings = legacyIsAccountWide and addon.accountSettings or addon.characterSettings
-        elseif legacyIsAccountWide then
-            settings = ZO_SavedVars:NewAccountWide(accountWideSavedVarsName, 1, nil, accountWideDefaults, copyToWorldName)
-        end
-        if settings then
-            self:DeepSavedVarsCopy(legacySavedVars, settings)
-        end
-    end
-    self:ClearSavedVars(legacySavedVars)
-    legacySavedVars.libSavedVarsMigrated = true
-end
-
-function libSavedVars:Set(addon, key, value)
-    if addon.characterSettings.useAccountSettings then
-        addon.accountSettings[key] = value
-    else
-        addon.characterSettings[key] = value
-    end 
 end
